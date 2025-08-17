6b82aa3c177ee2023e615e60b1ea26b948f9f3e4
jhoaking <peredoroj@gmail.com>
2025-08-17 12:20:36 -0400

    entitys del auth

---
+++ backend/src/auth/entities/auth.entity.ts
@@ -0,0 +1,41 @@
+import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn} from 'typeorm'
+
+
+
+@Entity({name : 'Users'})
+export class User {
+
+    @PrimaryGeneratedColumn('uuid')
+      @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  fullName: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  email: string;
+
+  @Column('text', {
+    select: true,
+  })
+  password: string;
+
+  @Column('bool', {
+    default: true,
+  })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column('text', {
+    array: true,
+    default: ['user'],
+  })
+  roles: string[];
+
+
+  
+}
