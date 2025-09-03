d15af3f0a3824bdd1c7e1ce29f6f65815846a23b
jhoaking <peredoroj@gmail.com>
2025-09-03 11:23:33 -0400

    documentacio nacabada

--- backend/src/auth/entities/auth.entity.ts
+++ backend/src/auth/entities/auth.entity.ts
@@ -7,76 +7,31 @@ import {
 } from 'typeorm';
 import { TwoFactor } from '../two-factor/entities/two-factor.entity';
 import { Task } from 'src/tasks/entities/task.entity';
-import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'Users' })
 export class User {
-
-  @ApiProperty({
-    description : 'id of user',
-    example : '1efcdae9-4507-496e-97c0-48685574dedc',
-    uniqueItems : true
-  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @ApiProperty({
-    description : 'name user',
-    example : 'Pepito Gomez'
-  })
   @Column('text')
   fullName: string;
 
-   @ApiProperty({
-    description : 'email of user',
-    example : 'papeto123@gmail.com',
-    uniqueItems : true
-  })
-  @Column('text', {
-    unique: true,
-  })
+  @Column('text', { unique: true })
   email: string;
 
-   @ApiProperty({
-    description : 'password of user',
-    example : 'Abc123'
-  })
-  @Column('text', {
-    select: true,
-  })
+  @Column('text', { select: true })
   password: string;
 
-   @ApiProperty({
-    description : 'see that the user this active',
-    example : true,
-  })
-  @Column('bool', {
-    default: true,
-  })
+  @Column('bool', { default: true })
   isActive: boolean;
 
-   @ApiProperty({
-    description : 'when the user was created',
-    example : '2025-09-13'
-  })
   @CreateDateColumn()
   createdAt: Date;
 
-   @ApiProperty({
-    description : 'role of user',
-    example : 'admin',
-  })
-  @Column('text', {
-    array: true,
-    default: ['user'],
-  })
+  @Column('text', { array: true, default: ['user'] })
   roles: string[];
 
-  @OneToMany(
-    () => TwoFactor,
-    (twoFactor) => twoFactor.user,
-    //cascade por si acaso
-  )
+  @OneToMany(() => TwoFactor, (twoFactor) => twoFactor.user)
   twoFactor: TwoFactor[];
 
   @OneToMany(() => Task, (task) => task.user)
--- backend/src/tasks/entities/task.entity.ts
+++ backend/src/tasks/entities/task.entity.ts
@@ -5,98 +5,38 @@ import {
   ManyToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
-import { ApiProperty } from '@nestjs/swagger';
-
 import { Priority, Status } from '../types/task';
-
 import { User } from '../../auth/entities/auth.entity';
-
-
-
 
 @Entity({ name: 'tasks' })
 export class Task {
-
-  @ApiProperty({
-    example : '1efcdae9-4507-496e-97c0-48685574defc',
-    description : 'task ID',
-    uniqueItems : true
-  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-
-  @ApiProperty({
-    example : 'Redactar informe mensual',
-    description : 'name for to task'
-  })
   @Column('text')
   name: string;
 
-  @ApiProperty({
-    example : 'redactar el informe de una migracion',
-    description : 'description of the task'
-  })
-  @Column('text', {
-    default: '',
-  })
+  @Column('text', { default: '' })
   description: string;
 
-
-  @ApiProperty({
-    example : '2025-08-09',
-    description : 'when to task was created '
-  })
   @CreateDateColumn()
   createdAt: Date;
 
-  @ApiProperty({
-    example : '2h',
-    description : 'duration of the task'
-  })
-  @Column('text', {
-    default: '0',
-  })
+  @Column('text', { default: '0' })
   duration: string;
 
-  @ApiProperty({
-    enum : Status,
-    description : 'status of the task as (pending,completed,delayed)'
-  })
-  @Column('enum', {
-    enum: Status,
-    default: Status.pending,
-  })
+  @Column('enum', { enum: Status, default: Status.pending })
   status: Status;
 
-   @ApiProperty({
-    enum : Priority,
-    description : 'priority of the task as (low,medium,high)'
-  })
-  @Column('enum', {
-    enum: Priority,
-    default: Priority.medium,
-  })
+  @Column('enum', { enum: Priority, default: Priority.medium })
   priority: Priority;
 
-  
-  @ApiProperty({
-    example : true,
-    description : 'see that to task was completed'
-  })
-  @Column('bool', {
-    default: false,
-  })
+  @Column('bool', { default: false })
   isCompleted: boolean;
 
-  @ApiProperty({
-    example : '2025-08-09',
-    description  : 'see when the task is due'
-  })
   @Column('timestamp')
   dueDate: Date;
 
-  @ManyToOne(() => User, (user) => user.task, {eager : true})
+  @ManyToOne(() => User, (user) => user.task, { eager: true })
   user: User;
- 
 }
